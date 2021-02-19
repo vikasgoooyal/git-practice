f730c12a4b7bdb409b256032d9242e9b9703cba5
vikas goyal <vikasgooyal@gmail.com>
2021-02-19 23:54:49 +0530

    new modification added

---
+++ src/Geek.java
@@ -0,0 +1,37 @@
+
+public class Geek {
+	Geek(String name)
+	{
+		System.out.println("name is"+ name);
+	}
+	Geek(int id)
+	{     
+
+
+
+
+
+
+
+
+
+
+
+		System.out.println("id is"+ id);
+	}
+	Geek(long salary)
+	{
+		System.out.println("salary is"+salary);
+		
+	}
+}
+class Bikas{
+	public static void main(String Arg[]) {
+		Geek v = new Geek("vikas");
+		Geek v1 = new Geek(250);
+		Geek v2 = new Geek(120512);
+		
+		
+		
+	}
+}
